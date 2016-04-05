d57f05119bdba1d29ab066bc543951c454520de7
ramon <ramonmulia@gmail.com>
2016-04-05 01:22:22 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var app = express();
+
+var informationRoute = require('./lib/information/information-routes')();
+
+app.use(cors({
+  origin: "*"
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use('/api/v1', informationRoute);
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
