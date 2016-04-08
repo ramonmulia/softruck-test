65556c327b96cace326441a496727cc690c2fee8
ramon <ramonmulia@gmail.com>
2016-04-07 21:39:02 -0300

    atualização readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# softruck-test
+# softruck-test - server
+
+Server to ANP web client.
+
+### Run
+Install dependencies and run server.
+```sh
+$ npm start
+```
+The server will run on: http://localhost:8008.
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 var express = require('express');
+var morgan = require("morgan");
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var timeout = require('connect-timeout');
 var db = require('./lib/db/db');
 var app = express();
-
 var informationRoute = require('./lib/information/information-routes')();
 
+app.use(timeout(120000000));
+app.use(morgan());
 app.use(cors({
-  origin: "*"
+  origin: "http://localhost:3000"
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-
 app.use('/api/v1', informationRoute);
-
 app.listen(8008, function() {
   db.connect(function() {
-    console.log('Server listening on port: '+8008);
+    console.log('Server listening on port: 8008');
   });
 });
